4d7cd07bc6f77c0113d3a758a11dffee67071805
Sebastian Sierra <sierrasebastian206@gmail.com>
2026-09-21 18:59:41 -0500

    feat: add book repository and service

---
+++ app/Services/BookService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+use App\Repositories\BookRepository;
+
+class BookService{
+    //Crea una nueva instancia del servicio
+    public function __construct(
+        private BookRepository $bookRepository
+    ){}
+
+    //Obtiene todos los libros registros en el sistema
+    public function getAll(){
+        return $this->bookRepository->getAll();
+    }
+
+    //Busca un libro especifico por su ID
+    public function findById(int $id){
+        return $this->bookRepository->findById($id);
+    }
+
+    //Crea un libro
+    public function create(array $data): Book{
+        return $this->BookRepository->create($data);
+    }
+
+    //Actualiza un libro
+    public function update(Book $book, array $data): Book{
+        return $this->BookRepository->update($book, $data);
+    }
+
+    //Eliminar un libro
+    public function delete (Book $book): bool{
+        return $this->BookRepository->delete($book);
+    }
+}
